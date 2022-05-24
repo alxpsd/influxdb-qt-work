a9f7446b54edc6355dc8f836cd803f1d9d6cff5f
alxpsd <iliveinthehouse17@gmail.com>
2022-05-24 09:50:57 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <QCoreApplication>
+#include <QObject>
+#include <QDebug>
+#include "influxlib/include/InfluxDBFactory.h"
+//#include "influxlib/include/InfluxDB.h"
+
+#include <iostream>
+
+
+
+int main(int argc,char *argv[])
+{
+    auto db = influxdb::InfluxDBFactory::Get("http://10.23.0.64:8086?db=temperature_db");
+    db->createDatabaseIfNotExists();
+    for (auto i: db->query("SHOW DATABASES")) std::cout<<i.getTags()<<std::endl;
+    return 0;
+}
